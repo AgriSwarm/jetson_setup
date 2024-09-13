bf2d26cad314833b4540c5a81559998bdf119eda
tomoking <arita.tomoki@keio.jp>
2024-09-13 16:59:18 +0900

    sudo

--- d2slam/ceres_install.sh
+++ d2slam/ceres_install.sh
@@ -4,9 +4,12 @@ set -e
 
 cd $HOME/workspace/setup || exit 1
 
-git clone https://github.com/HKUST-Swarm/ceres-solver -b D2SLAM && \
+if [ ! -d "ceres-solver" ]; then
+    git clone https://github.com/HKUST-Swarm/ceres-solver -b D2SLAM
+fi
+
 cd ceres-solver && \
-mkdir build && \
+mkdir -p build && \
 cd build && \
-sudo cmake  -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARKS=OFF -DCUDA=OFF .. && \
+sudo cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARKS=OFF -DCUDA=OFF .. && \
 sudo make -j2 install
