af9fe50be5340a382452a9a8d80e41104fc23b7a
AritaTomoki <arita.tomoki@keio.jp>
2024-09-14 10:56:50 +0900

    ddynamic

--- utils/ros_install.sh
+++ utils/ros_install.sh
@@ -13,7 +13,6 @@ gecho() {
 gecho "Start installing ROS.."
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && \
-sudo apt-get update && \
 sudo apt-get install -y curl && \
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - && \
 sudo apt-get update && \
@@ -23,7 +22,7 @@ source ~/.bashrc && \
 sudo apt-get install -y python3-rosdep python3-rosinstall \
     python3-rosinstall-generator python3-wstool build-essential \
     ros-${ROS_VERSION}-ros-numpy ros-${ROS_VERSION}-rviz-visual-tools \
-    ros-${ROS_VERSION}-mavros* && \
+    ros-${ROS_VERSION}-mavros* ros-${ROS_VERSION}-ddynamic-reconfigure && \
 gecho "Initialize rosdep" && \
 sudo rosdep init && \
 rosdep update && \
